94239163bea65cdccdca3640bf6351404a50f083
Am-iir <maharjanamir.101@gmail.com>
2023-08-09 14:20:40 +1000

    Resource based level work

--- cost_metric_lambda.py
+++ cost_metric_lambda.py
@@ -6,39 +6,14 @@ from botocore.exceptions import ClientError
 
 
 def handler(event, context):
-    bucket_name = os.environ['BUCKET']
-    directory_name = 'cost-metrics'
-    file_name = f'{directory_name}/total_account_cost.json'
-
-    # Get the JSON file from S3
-    s3_client = boto3.client('s3')
     try:
-        response = s3_client.get_object(Bucket=bucket_name, Key=file_name)
-        json_data = response['Body'].read().decode('utf-8')
 
-        # Parse the JSON data to extract the cost information
-        cost_data = json.loads(json_data)
+        handle_total_account()
 
-        # Get the current month in the format "January", "February", etc.
-        current_month = datetime.utcnow().strftime('%B')
-
-        # Get the account ID from the cost_data dictionary
-        account_id = list(cost_data.keys())[0]
-
-        # Get the total cost for the current month
-        total_cost = cost_data[account_id].get(current_month, 0.0)
-
-        # Get the maximum budget from environment variables
-        maximum_budget = float(os.environ['MAXIMUM_BUDGET'])
-
-        # Calculate the cost percentage based on the maximum budget
-        cost_percentage = (total_cost / maximum_budget) * 100
-
-        # Publish the cost percentage as a custom metric to CloudWatch Metrics
-        publish_to_cloudwatch(cost_percentage)
+        handle_resource_cost()
 
         # Publish the alarm to the SNS topic along with the calculated values
-        publish_to_sns(cost_percentage, maximum_budget, total_cost)
+        # publish_to_sns(cost_percentage, maximum_budget, total_cost)
 
         return {
             'statusCode': 200,
@@ -60,12 +35,74 @@ def handler(event, context):
             }
 
 
-def publish_to_cloudwatch(cost_percentage):
+def calculate_cost_percentage(cost, maximum_budget):
+    if maximum_budget > 0:
+        return (cost / maximum_budget) * 100
+    else:
+        return 0.0
+
+
+def get_json_data(file_name):
+    bucket_name = os.environ['BUCKET']
+    directory_name = 'cost-metrics'
+
+    json_file = f'{directory_name}/{file_name}'
+
+    s3_client = boto3.client('s3')
+    response = s3_client.get_object(Bucket=bucket_name, Key=json_file)
+    json_data = response['Body'].read().decode('utf-8')
+    # Parse the JSON data to extract the cost information
+    return json.loads(json_data)
+
+
+def handle_total_account():
+    total_cost_file_name = 'total_account_cost.json'
+
+    total_account_cost_data = get_json_data(total_cost_file_name)
+
+    # Get the current month in the format "January", "February", etc.
+    current_month = datetime.utcnow().strftime('%B')
+
+    # Get the account ID from the cost_data dictionary
+    account_id = list(total_account_cost_data.keys())[0]
+
+    # Get the total cost for the current month
+    total_cost = total_account_cost_data[account_id].get(current_month, 0.0)
+
+    maximum_budget = float(os.environ['MAXIMUM_BUDGET'])
+
+    cost_percentage = calculate_cost_percentage(total_cost, maximum_budget)
+
+    # Publish the cost percentage as a custom metric to CloudWatch Metrics
+
+    publish_to_cloudwatch(os.environ['METRIC_NAME'], cost_percentage)
+    publish_to_sns(cost_percentage, maximum_budget, total_cost)
+
+
+def handle_resource_cost():
+    file_name = 'resource_based_cost.json'
+    maximum_budget = float(os.environ['MAXIMUM_BUDGET'])
+
+    resources_data = get_json_data(file_name)
+
+    for resource in resources_data:
+        service = resource.get('Service')
+        cost = float(resource.get('Cost', 0.0))
+
+        # Calculate the cost percentage based on the maximum budget
+        cost_percentage = calculate_cost_percentage(cost, maximum_budget)
+
+        # Process the data as needed (e.g., push to CloudWatch, generate alarms, etc.)
+        if cost_percentage > 0:
+            publish_to_cloudwatch(service, cost_percentage)
+            # Generate alarms here if required based on cost_percentage and the threshold
+
+
+def publish_to_cloudwatch(metric_name, cost_percentage):
     cloudwatch = boto3.client('cloudwatch')
 
-    # Get the namespace and metric name from environment variables
+    # Get the namespace from environment variables
     cloudwatch_namespace = os.environ['CLOUDWATCH_NAMESPACE']
-    metric_name = os.environ['METRIC_NAME']
 
     # Publish the cost percentage as a custom metric
     cloudwatch.put_metric_data(
--- resources.tf
+++ resources.tf
@@ -13,16 +13,40 @@ resource "aws_sns_topic_subscription" "lambda_subscription" {
 # Create a CloudWatch metric alarm to trigger the SNS topic
 /*
 */
-resource "aws_cloudwatch_metric_alarm" "cost_percentage_alarm" {
-  alarm_name          = "${var.namespace}-Cost Percentage Exceeds Threshold 8"
+resource "aws_cloudwatch_metric_alarm" "total_cost_percentage_alarm" {
+  alarm_name          = "${var.namespace}- TotalCost PercentageAlarm"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
   metric_name         = var.metric_name
   namespace           = var.cloudwatch_namespace
   period              = 86400  # 1 day (in seconds)
-  statistic           = "Average"
+  statistic           = "Maximum"
   threshold           = var.threshold # Adjust the threshold value as needed
   alarm_description   = "The cost percentage has exceeded the threshold"
   alarm_actions       = [aws_sns_topic.alarm_topic.arn]
 
+}
+
+locals {
+  resources_list = jsondecode(data.aws_s3_object.resources_data.body)
+}
+
+data "aws_s3_object" "resources_data" {
+  bucket = var.bucket
+  key    = "cost-metrics/resource_based_cost.json"
+}
+
+resource "aws_cloudwatch_metric_alarm" "resource_cost_percentage_alarm" {
+  for_each = { for idx, item in local.resources_list : idx => item.Service }
+
+  alarm_name          = "${each.value}-ResourceCost PercentageAlarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = each.value  # Use the Service attribute as the metric name
+  namespace           = var.cloudwatch_namespace
+  period              = 86400  # 1 day (in seconds)
+  statistic           = "Maximum"
+  threshold           =  var.threshold
+  alarm_description   = "The cost percentage has exceeded the threshold"
+  alarm_actions       = [aws_sns_topic.alarm_topic.arn]
 }
